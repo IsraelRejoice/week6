0d2ee6cbf5a0ae3659257abb3b4aa4c315c28cca
IsraelRejoice <112409082+IsraelRejoice@users.noreply.github.com>
2022-11-22 17:42:09 +0100

    Update app.js

--- app.js
+++ app.js
@@ -17,13 +17,13 @@ carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 //button listeners
 nextBtn.addEventListener('click', ()=>{
     if(counter >= carouselImages.length -1) return;
-    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    carouselSlide.style.transition = "transform 1s ease-in-out";
     counter++;
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 });
 prevBtn.addEventListener('click', ()=>{
     if(counter <= 0) return;
-    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    carouselSlide.style.transition = "transform 1s ease-in-out";
     counter--;
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 });
@@ -35,10 +35,10 @@ carouselSlide.addEventListener('transitionend', ()=> {
         carouselSlide.style.transition = "none";
         counter = carouselImages.length - 2;
         carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-    };
+    }
     if(carouselImages[counter].id === 'first-clone'){
         carouselSlide.style.transition = "none";
         counter = carouselImages.length - counter;
         carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-    };
+    }
 });
